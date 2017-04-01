ae22dbb4bbe5572af0b680f5b2a69d38ea8a5b8d
Colton Kelly <coltonkelly33@gmail.com>
2017-04-01 02:14:23 -0400

    Actually added Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,76 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by Colton on 4/1/2017.
  */
 public class Game {
+
+    private int numPlayers;
+    private Player[] players;
+    private Die die;
+    private CardDeck cardDeck;
+
+
+    public Game(Player[] players) throws IOException {
+
+        this.players = players;
+        this.numPlayers = players.length;
+
+        this.die = new Die();
+        this.cardDeck = new CardDeck();
+
+        setPlayersTurnOrder(players);
+
+    }
+
+    // This for-loop triple threat monstrosity is one way to solve the
+    // "who goes first" RNG requirement without worrying about duplicates
+    private void setPlayersTurnOrder(Player[] players) {
+
+        ArrayList<Integer> turns = new ArrayList<>(numPlayers);
+
+        for (int i = 0; i < this.numPlayers; i++)
+            turns.add(i);
+
+        // it's random?
+        Collections.shuffle(turns);
+
+        // Dish out the shuffled turns
+        for (int i = 0; i < numPlayers; i++)
+            players[i].setTurnOrder(turns.get(i));
+
+        // rearrange the players array to be in turn order instead
+        // of creation order
+        for (int i = 0; i < numPlayers; i++) {
+
+            if (players[i].getTurnOrder() != i) {
+
+                Player temp = players[i];
+                players[i] = players[players[i].getTurnOrder()];
+                players[temp.getTurnOrder()] = temp;
+
+            }
+        }
+    }
+
+    // this is just a test drive. I'm assuming we'll get player array from
+    // the GUI class
+    /*public static void main(String[] args) throws IOException {
+
+        Player[] testPlayers = new Player[3];
+
+        for (int i = 0; i < testPlayers.length; i++) {
+
+            testPlayers[i] = new Player();
+            testPlayers[i].setPlayerName("Shia LaBeouf" + Integer.toString(i));
+
+        }
+
+        Game game = new Game(testPlayers);
+
+        for (Player player : testPlayers)
+            System.out.println(player.getPlayerName() + " " + player.getTurnOrder());
+
+    }*/
 }
